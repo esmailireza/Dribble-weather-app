e901efc277a7617fd6b1c883175026ff59911727
Your Name <you@example.com>
2021-12-15 20:16:35 +0330

    fixed chart

--- app.js
+++ app.js
@@ -6,24 +6,14 @@ const myChart = new Chart(ctx, {
     datasets: [
       {
         label: "Temperature",
-        data: [20, 10, 0, -10, -20],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
+        data: [0, 10, -10, 20, -20],
         borderWidth: 1,
+        fill: true,
+        tension: 0.4,
+        pointRadius: 0,
+        borderColor: "#7285ff",
+        backgroundColor: "#7285ff38",
+        responsive: true,
       },
     ],
   },
@@ -31,6 +21,14 @@ const myChart = new Chart(ctx, {
     scales: {
       y: {
         beginAtZero: true,
+      },
+      maintainAspectRatio: false,
+    },
+    plugins: {
+      title: {
+        display: true,
+        text: " Temperature . Precipitation . Wind",
+        align: "start",
       },
     },
   },
